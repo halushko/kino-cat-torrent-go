6f197ee8d7807c9f61e440af7132578950cd67f3
Dmytro Galushko <galushko@netcracker.com>
2024-10-14 21:32:22 +0300

    Add Get all torrents processor

--- listeners/get_all_torrents.go
+++ listeners/get_all_torrents.go
@@ -3,12 +3,15 @@ package listeners
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/halushko/kino-cat-core-go/nats_helper"
 	"github.com/hekmon/transmissionrpc/v2"
 	"github.com/nats-io/nats.go"
 	"log"
+	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type CommandNatsMessage struct {
@@ -54,6 +57,7 @@ func GetAllTorrents() {
 
 			log.Printf("[GetAllTorrents] Торенти отримано")
 
+			generateAnswer(torrents)
 			for _, torrent := range torrents {
 				log.Printf("ID: %d, Назві: %s\n", *torrent.ID, *torrent.Name)
 			}
@@ -67,4 +71,76 @@ func GetAllTorrents() {
 	}
 
 	nats_helper.StartNatsListener("EXECUTE_TORRENT_COMMAND_LIST", listener)
+}
+
+func generateAnswer(torrents []transmissionrpc.Torrent) string {
+	var line strings.Builder
+	line.WriteString("||")
+
+	for _, torrent := range torrents {
+		line.WriteString(
+			fmt.Sprintf("%s %s\n%s %s\n%s %s\n",
+				getStatusIcon(torrent), *torrent.Name,
+				getProgressBar(torrent), getGigabytesLeft(torrent),
+				fmt.Sprintf("%s%s%s%d", "/more_", "", "_", int(*torrent.ID)),
+				fmt.Sprintf("%s%s%s%d", "/files_", "", "_", int(*torrent.ID)),
+			),
+		)
+	}
+	return line.String()
+}
+
+func getProgressBar(torrent transmissionrpc.Torrent) string {
+	blocks := 20
+	blackBlocks := int(*torrent.PercentDone * float64(blocks))
+	var line strings.Builder
+	line.WriteString("||")
+	for i := 0; i < blackBlocks; i++ {
+		line.WriteString("█")
+	}
+	if blackBlocks < blocks {
+		line.WriteString("▒")
+	}
+	if blackBlocks+1 < blocks {
+		for i := blackBlocks + 1; i < blocks; i++ {
+			line.WriteString("░")
+		}
+	}
+	return line.String()
+}
+
+func getGigabytesLeft(torrent transmissionrpc.Torrent) string {
+	done := *torrent.PercentDone
+	if done == 1.0 {
+		return " (заверш)"
+	}
+
+	percentDone := math.Round(done * 100)
+	totalSize := float64(*torrent.TotalSize)
+	remainingSize := math.Round((totalSize-(totalSize*done))/1000000.0) / 1000.0
+
+	return fmt.Sprintf("%.0f %% (%.3f Gb залиш)", percentDone, remainingSize)
+}
+
+func getStatusIcon(torrent transmissionrpc.Torrent) string {
+	switch *torrent.Status {
+	case transmissionrpc.TorrentStatusStopped:
+		return "⏸"
+	case transmissionrpc.TorrentStatusCheckWait:
+		return "⏲♾"
+	case transmissionrpc.TorrentStatusCheck:
+		return "♾"
+	case transmissionrpc.TorrentStatusDownloadWait:
+		return "⏲⬇️"
+	case transmissionrpc.TorrentStatusDownload:
+		return "⬇️"
+	case transmissionrpc.TorrentStatusSeedWait:
+		return "⏲⬆️"
+	case transmissionrpc.TorrentStatusSeed:
+		return "♻️"
+	case transmissionrpc.TorrentStatusIsolated:
+		return "🈲"
+	default:
+		return "🈲"
+	}
 }

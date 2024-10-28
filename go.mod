4185a9b17ab0f601042b4e620cf3eaf8aae3dc2f
Dmytro Galushko <galushko@netcracker.com>
2024-10-28 08:02:14 +0200

    Adapt to core:v1.15.2 update

--- go.mod
+++ go.mod
@@ -3,17 +3,14 @@ module kino-cat-torrent-go
 go 1.23.2
 
 require (
-	github.com/halushko/kino-cat-core-go v0.0.0-20241022200643-a3598ab4c343
+	github.com/halushko/kino-cat-core-go v0.0.0-20241027083015-dd9bb3601be0
 	github.com/hekmon/transmissionrpc/v2 v2.0.1
-	github.com/nats-io/nats.go v1.37.0
 )
 
 require (
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hekmon/cunits/v2 v2.1.0 // indirect
 	github.com/klauspost/compress v1.17.2 // indirect
-	github.com/nats-io/nkeys v0.4.7 // indirect
-	github.com/nats-io/nuid v1.0.1 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 )
--- helpers/transmission_helper.go
+++ helpers/transmission_helper.go
@@ -1,21 +1,24 @@
 package helpers
 
 import (
-	"encoding/json"
 	"github.com/halushko/kino-cat-core-go/nats_helper"
 	"github.com/halushko/kino-cat-core-go/warehouse_helper"
 	"github.com/hekmon/transmissionrpc/v2"
-	"github.com/nats-io/nats.go"
 	"log"
 	"sort"
 	"strconv"
 )
 
-const OutputQueue = "TELEGRAM_OUTPUT_TEXT_QUEUE"
+const outputQueue = "TELEGRAM_OUTPUT_TEXT_QUEUE"
 
 func ListenToNatsMessages(queue string, f func(key string, args []string, client *transmissionrpc.Client) string) {
-	processor := func(msg *nats.Msg) {
-		executeForServers(msg, f)
+	processor := func(data []byte) {
+		userId, args, err := nats_helper.ParseNatsBotCommand(data)
+		if err != nil {
+			log.Printf("[ListenToNatsMessages] Помилка під час прослуховування черги \"%s\" NATS: %v", queue, err)
+			return
+		}
+		executeForServers(userId, args, f)
 	}
 	listener := &nats_helper.NatsListenerHandler{
 		Function: processor,
@@ -26,70 +29,52 @@ func ListenToNatsMessages(queue string, f func(key string, args []string, client
 	}
 }
 
-func executeForServers(msg *nats.Msg, f func(key string, args []string, client *transmissionrpc.Client) string) {
-	clients, command := connectToTransmission(msg)
-	keys := make([]string, 0)
-	for key := range clients {
-		keys = append(keys, key)
-	}
-	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
-	for _, key := range keys {
-		log.Printf("[ExecuteForServers] Старт роботи зі сховищем %s", key)
-		SendAnswer(command.UserId, f(key, command.Arguments, clients[key]))
-	}
-}
-
-func connectToTransmission(msg *nats.Msg) (map[string]*transmissionrpc.Client, nats_helper.Command) {
-	log.Printf("[ConnectToTransmission] Отримано повідомлення з NATS: %s", string(msg.Data))
-
-	var inputMessage nats_helper.Command
-	if err := json.Unmarshal(msg.Data, &inputMessage); err != nil {
-		log.Printf("[ConnectToTransmission] Помилка при розборі повідомлення з NATS: %v", err)
-		return nil, inputMessage
-	}
-
-	log.Printf("[ConnectToTransmission] Парсинг повідомлення: chatID = %d, arguments = %s", inputMessage.UserId, inputMessage.Arguments)
-
-	if inputMessage.UserId != 0 {
-		var clients = make(map[string]*transmissionrpc.Client)
-		for key, value := range getTransmissionServers(inputMessage.Arguments) {
-			ip := value.IP
-			port, err := strconv.ParseInt(value.Port, 10, 64)
-
-			if err != nil {
-				log.Printf("[ConnectToTransmission] Помилка, порт Transmission задано невірно для сховища %s %v", key, err)
-				continue
-			}
-
-			client, err := transmissionrpc.New(ip, "", "", &transmissionrpc.AdvancedConfig{
-				Port:  uint16(port),
-				HTTPS: false,
-			})
-			if err != nil {
-				log.Printf("[ConnectToTransmission] Помилка підключенні до transmission для сховища %s: %v", key, err)
-				continue
-			}
-			clients[key] = client
-		}
-		return clients, inputMessage
-	} else {
-		log.Printf("[ConnectToTransmission] Помилка: ID користувача порожній")
-	}
-	return nil, inputMessage
-}
-
 func SendAnswer(userId int64, message string) {
-	log.Printf("[SendAnswer] Answer:%s", message)
-	if request, errMarshal := json.Marshal(nats_helper.NatsMessage{
-		UserId: userId,
-		Text:   message,
-	}); errMarshal == nil {
-		if errPublish := nats_helper.PublishToNATS(OutputQueue, request); errPublish != nil {
-			log.Printf("[SendAnswer] ERROR in publish to %s:%s", OutputQueue, errPublish)
-		}
-	} else {
-		log.Printf("[SendAnswer] ERROR in publish to %s:%s", OutputQueue, errMarshal)
+	log.Printf("[SendAnswer] Відповідь: %s", message)
+	err := nats_helper.PublishTextMessage(outputQueue, userId, message)
+	if err != nil {
+		log.Printf("[SendAnswer] Помилка: %v", err)
 	}
+}
+
+func executeForServers(userId int64, args []string, f func(key string, args []string, client *transmissionrpc.Client) string) {
+	clients := connectToTransmission(userId, args)
+
+	warehouses := make([]string, 0)
+	for key := range clients {
+		warehouses = append(warehouses, key)
+	}
+	sort.Slice(warehouses, func(i, j int) bool { return warehouses[i] < warehouses[j] })
+
+	for _, key := range warehouses {
+		log.Printf("[ExecuteForServers] Старт роботи зі сховищем %s", key)
+		SendAnswer(userId, f(key, args, clients[key]))
+	}
+}
+
+func connectToTransmission(userId int64, args []string) map[string]*transmissionrpc.Client {
+	if userId == 0 {
+		log.Printf("[ConnectToTransmission] Помилка: ID користувача порожній")
+		return nil
+	}
+
+	var clients = make(map[string]*transmissionrpc.Client)
+	for key, value := range getTransmissionServers(args) {
+		port, err := strconv.ParseInt(value.Port, 10, 64)
+		if err != nil {
+			log.Printf("[ConnectToTransmission] Помилка, порт Transmission задано невірно для сховища %s %v", key, err)
+			continue
+		}
+
+		client, err := transmissionrpc.New(value.IP, "", "", &transmissionrpc.AdvancedConfig{Port: uint16(port), HTTPS: false})
+		if err != nil {
+			log.Printf("[ConnectToTransmission] Помилка підключенні до transmission для сховища %s: %v", key, err)
+			continue
+		}
+
+		clients[key] = client
+	}
+	return clients
 }
 
 func getTransmissionServers(commandArguments []string) map[string]warehouse_helper.WarehouseConfig {
